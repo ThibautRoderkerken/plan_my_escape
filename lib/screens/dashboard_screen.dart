34928c0551474f3bf4633b90180f2dbec441a113
Thibaut RODERKERKEN BOGAERT <t.roderkerkenbogaert@student.helmo.be>
2023-10-12 09:09:31 +0200

    M2 - Première version avec trois champs texte et un bouton.

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -10,10 +10,49 @@ class DashboardScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Tableau de bord'),
       ),
-      body: const Center(
-        child: Text('Bienvenue sur le tableau de bord !'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Ajouter une nouvelle période de vacances',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 24),
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Destination',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Date de début',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.datetime,
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Date de fin',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.datetime,
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Logique pour ajouter la période de vacances
+              },
+              child: const Text('Ajouter'),
+            ),
+          ],
+        ),
       ),
-      drawer: const AppNavigationDrawer (),
+      drawer: const AppNavigationDrawer(),
     );
   }
 }
