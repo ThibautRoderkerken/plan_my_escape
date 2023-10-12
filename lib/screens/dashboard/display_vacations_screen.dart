c59a868e6dac23ed5402aeb7398892cc15478598
Thibaut RODERKERKEN BOGAERT <t.roderkerkenbogaert@student.helmo.be>
2023-10-12 11:02:45 +0200

    M2 - Ajout d'un titre pour la nouvelle section.

--- lib/screens/dashboard/display_vacations_screen.dart
+++ lib/screens/dashboard/display_vacations_screen.dart
@@ -8,34 +8,50 @@ class DisplayVacationsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: dashboardViewModel.vacationPeriods.length,
-      itemBuilder: (context, index) {
-        final vacation = dashboardViewModel.vacationPeriods[index];
-        return Card(
-          margin: const EdgeInsets.all(10),
-          elevation: 5,
-          child: ListTile(
-            leading: const Icon(Icons.beach_access),
-            title: Text(vacation.destination),
-            subtitle: Text(
-                "${vacation.startDate.toLocal().toString().split(' ')[0]} - ${vacation.endDate.toLocal().toString().split(' ')[0]}\n"
-                    "${vacation.weatherInfo.description}, ${vacation.weatherInfo.temperature}°C"
+    return Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            'Vos périodes de vacances planifiées',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
             ),
-            trailing: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(Icons.group),
-                Text(vacation.members.length.toString()), // Nombre de membres
-                const SizedBox(height: 4),
-                const Icon(Icons.event),
-                Text(vacation.activities.length.toString()), // Nombre d'activités
-              ],
-            ),
-            // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => VacationDetailScreen(vacation: vacation))), // Pour une future page de détails
           ),
-        );
-      },
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: dashboardViewModel.vacationPeriods.length,
+            itemBuilder: (context, index) {
+              final vacation = dashboardViewModel.vacationPeriods[index];
+              return Card(
+                margin: const EdgeInsets.all(10),
+                elevation: 5,
+                child: ListTile(
+                  leading: const Icon(Icons.beach_access),
+                  title: Text(vacation.destination),
+                  subtitle: Text(
+                      "${vacation.startDate.toLocal().toString().split(' ')[0]} - ${vacation.endDate.toLocal().toString().split(' ')[0]}\n"
+                          "${vacation.weatherInfo.description}, ${vacation.weatherInfo.temperature}°C"
+                  ),
+                  trailing: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(Icons.group),
+                      Text(vacation.members.length.toString()), // Nombre de membres
+                      const SizedBox(height: 4),
+                      const Icon(Icons.event),
+                      Text(vacation.activities.length.toString()), // Nombre d'activités
+                    ],
+                  ),
+                  // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => VacationDetailScreen(vacation: vacation))), // Pour une future page de détails
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
