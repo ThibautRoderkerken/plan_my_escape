4e0badd3f86294694971d9af8926098143e1457f
Thibaut RODERKERKEN BOGAERT <t.roderkerkenbogaert@student.helmo.be>
2023-11-27 15:53:09 +0100

    API  - Ajout d'une nouvelle méthode qui va permettre de mettre a jour une période de vacance.

--- lib/services/holiday_service.dart
+++ lib/services/holiday_service.dart
@@ -16,7 +16,6 @@ class HolidayService {
     try {
       SharedPreferences prefs = await SharedPreferences.getInstance();
       String? cookie = prefs.getString('cookie');
-      print('Cookie: $cookie');
 
       final response = await http.post(
         Uri.parse(baseUrl),
@@ -73,6 +72,59 @@ class HolidayService {
       if (response.statusCode == 200) {
         Iterable l = json.decode(response.body);
         return List<VacationPeriod>.from(l.map((model) => VacationPeriod.fromJson(model)));
+      } else {
+        switch (response.statusCode) {
+          case 400:
+            throw BadRequestException('Bad request');
+          case 404:
+            throw NotFoundException('Resource not found');
+          case 500:
+            throw InternalServerException('Internal server error');
+          default:
+            throw NetworkException('Unknown network error');
+        }
+      }
+    } on TimeoutException {
+      throw NetworkException('Network timeout');
+    } catch (e) {
+      if (e is! Exception) {
+        throw NetworkException('Network error: $e');
+      }
+      rethrow;
+    }
+  }
+
+  Future<dynamic> updateVacationPeriod(VacationPeriod vacation) async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? cookie = prefs.getString('cookie');
+
+      final response = await http.put(
+        Uri.parse('$baseUrl/${vacation.vacationIndex}'), // Utiliser l'ID de la période de vacances
+        headers: {
+          'Content-Type': 'application/json',
+          if (cookie != null) 'Cookie': cookie,
+        },
+        body: jsonEncode({
+          'Id': vacation.vacationIndex,
+          'Destination': vacation.destination,
+          'Start_at': vacation.startDate.toIso8601String().split('T')[0],
+          'End_at': vacation.endDate.toIso8601String().split('T')[0],
+          'Activities': vacation.activities
+              .map((activity) => {
+            'Name': activity.name,
+            'Description': activity.description,
+            'Destination': activity.address, // Utiliser 'address' pour la destination de l'activité
+            'Start_at': activity.scheduledDate?.toIso8601String().split('T')[0],
+            'End_at': activity.scheduledDate?.add(activity.duration ?? const Duration(hours: 0)).toIso8601String().split('T')[0],
+          })
+              .toList(),
+          'Users': []  // Ajoutez des informations sur les utilisateurs si nécessaire
+        }),
+      ).timeout(const Duration(seconds: 3));
+
+      if (response.statusCode == 200) {
+        return json.decode(response.body);
       } else {
         switch (response.statusCode) {
           case 400:
